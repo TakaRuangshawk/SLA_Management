65c8f879467644819d05b3ffcfc048c4b025fa73
Theerapat64003 <theerapatnugud@gmail.com>
2023-03-24 11:32:05 +0700

    new b

---
+++ Models/InsertListFileComLog.cs
@@ -0,0 +1,11 @@
+﻿namespace SLA_Management.Models
+{
+    public class InsertListFileComLog
+    {
+        public int Id { get; set; } = 0;
+        public string TERM_ID { get; set; }
+        public string ComLog { get; set; }
+        public string FileServer { get; set; }
+        public string TOTAL_RECORD { get; set; }
+    }
+}
