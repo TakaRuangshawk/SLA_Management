77b8bbdeb8fe06ed0596ab45c1c79c1747708fa1
ธนกฤต หิรัญรักษ์ <thanakritinw@gmail.com>
2023-03-24 16:01:07 +0700

    refactor code and add config

---
+++ Models/TermProbModel/Connect_MySqlModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SLA_Management.Models.TermProbModel
+{
+    public class Connect_MySqlModel
+    {
+        public string IP { get; set; }
+
+        public string Password { get; set; }
+
+        public string Port { get; set; }
+
+        public string Username { get; set; }
+
+        public string DBName { get; set; }
+
+        public string TimeOut { get; set; }
+
+        public string Pool { get; set; }
+
+        public string FullNameConnection { get; set; }
+    }
+}
