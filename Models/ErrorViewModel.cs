31937deee5c08dadf3806846093a79ed1d6b7a8f
TakaRuangshawk <takaruangs@outlook.com>
2023-03-30 14:16:29 +0700

    for gitignore

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SLA_Management.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
