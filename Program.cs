96a7c634cd31b88964a5749dd65afcd56baf78d2
TakaRuangs <104978628+TakaRuangshawk@users.noreply.github.com>
2024-03-25 00:11:51 +0700

    init for don't use home as first page

--- Program.cs
+++ Program.cs
@@ -31,6 +31,6 @@ app.MapHub<RPTHub>("/JobRPTHub");
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Operation}/{action=TicketManangement}/{id?}");
 
 app.Run();
